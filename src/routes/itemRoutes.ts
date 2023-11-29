3f90e46f54dcb503e88fad4809e67a75a37bee38
Dilum Harshana <dilum.harshana@itelasoft.com>
2023-11-29 11:48:04 +0530

    improvement - integrated prisma read and update operations to the application

--- src/routes/itemRoutes.ts
+++ src/routes/itemRoutes.ts
@@ -1,11 +1,19 @@
 import express from "express";
-import { addItems } from "../services/item_services.js";
+import {
+  addItems,
+  getItem,
+  getItems,
+  updateItem
+} from "../services/item_services.js";
 
 const router = express.Router();
 
-router.post(
-  "/add-item",
-  async (req, res) => await addItems(req, res)
-);
+router.get("/", async (req, res) => await getItems(req, res));
+
+router.get("/:id", async (req, res) => await getItem(req, res));
+
+router.post("/add-item", async (req, res) => await addItems(req, res));
+
+router.put("/", async (req, res) => await updateItem(req, res));
 
 export default router;
--- src/services/item_services.ts
+++ src/services/item_services.ts
@@ -1,10 +1,9 @@
-import { facebook_dummy_data } from "../data-files/facebook-dummy-data.js";
 import { generateResponse } from "../helpers/helpers.js";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-// dummy facebook data feting service
+// add an item to the items table
 export const addItems = async (req, res) => {
   const { name, unitPrice } = req.body;
 
@@ -20,5 +19,68 @@ export const addItems = async (req, res) => {
   } catch (error) {
     console.log(error);
     return generateResponse(res, null);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+// get all items from items table
+export const getItems = async (req, res) => {
+  try {
+    const response = await prisma.items.findMany();
+
+    return generateResponse(res, response);
+  } catch (error) {
+    console.log(error);
+    return generateResponse(res, null);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+// get a single item by id from items table
+export const getItem = async (req, res) => {
+  const itemId = req.params.id || null;
+
+  if (!itemId) return;
+
+  try {
+    const response = await prisma.items.findFirst({
+      where: {
+        id: parseInt(itemId)
+      }
+    });
+
+    return generateResponse(res, response);
+  } catch (error) {
+    console.log(error);
+    return generateResponse(res, null);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+// get a single item by id from items table
+export const updateItem = async (req, res) => {
+  const { itemId, itemName } = req.body || null;
+
+  if (!itemId) return;
+
+  try {
+    const response = await prisma.items.update({
+      where: {
+        id: parseInt(itemId)
+      },
+      data: {
+        name: itemName
+      }
+    });
+
+    return generateResponse(res, response);
+  } catch (error) {
+    console.log(error);
+    return generateResponse(res, null);
+  } finally {
+    await prisma.$disconnect();
   }
 };
